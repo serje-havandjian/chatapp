14195be1d603d41056657ded05a9d21c616a3177
Serje <Serje.havandjian@gmail.com>
2022-09-03 12:04:21 -0700

    SPIKE-commented out existing Conversation, rebuilding from scratch

--- chatroom/src/Conversation.js
+++ chatroom/src/Conversation.js
@@ -2,120 +2,120 @@ import { useState, useEffect } from "react"
 import { useParams } from "react-router-dom"
 import {createConsumer} from "@rails/actioncable"
 
-// function Conversation(){
+function Conversation(){
 
-//   const params = useParams()
+  const params = useParams()
   
 //   console.log(params.id)
 
-//   return(
-//     <p> Hello World</p>
-//   )
-// }
+  return(
+    <p> Hello World</p>
+  )
+}
 
-// export default Conversation
+export default Conversation
 
-function Conversation({conversation, user, newMessage, setNewMessage, setConversation, chatroomId}){
+// function Conversation({conversation, user, newMessage, setNewMessage, setConversation, chatroomId}){
 
-  const [displayChatsInConversation, setDisplayChatsInConversation ] = useState([])
+//   const [displayChatsInConversation, setDisplayChatsInConversation ] = useState([])
   
-  const params = useParams()
+//   const params = useParams()
   
-  console.log(params.id)
+//   console.log(params.id)
   
-  useEffect(()=>{
-    console.log("inside useEffect")
-    const fetchData = async () =>{
-      console.log("inside conversation useEffect fetch Data Function")
-      const data = await fetch(`/conversations/${params.id}`)
-      .then(result =>result.json())
-      .then((result) => setDisplayChatsInConversation(result))
-      .then((result) => {
-        console.log("in fetchData block", displayChatsInConversation)
-      })
-    }
+//   useEffect(()=>{
+//     console.log("inside useEffect")
+//     const fetchData = async () =>{
+//       console.log("inside conversation useEffect fetch Data Function")
+//       const data = await fetch(`/conversations/${params.id}`)
+//       .then(result =>result.json())
+//       .then((result) => setDisplayChatsInConversation(result))
+//       .then((result) => {
+//         console.log("in fetchData block", displayChatsInConversation)
+//       })
+//     }
 
-    fetchData()
+//     fetchData()
 
-    console.log(displayChatsInConversation)
-  },[])
+//     console.log(displayChatsInConversation)
+//   },[])
 
-  console.log(displayChatsInConversation)
+//   console.log(displayChatsInConversation)
 
-  // const showConversation = displayChatsInConversation.messages.map((message)=>{
-  //   return message.content
-  // })
+//   // const showConversation = displayChatsInConversation.messages.map((message)=>{
+//   //   return message.content
+//   // })
   
  
 
 
-  //  // STUPID ACTION CABLE STUFF
-  // useEffect(()=>{
-  //   const cable = createConsumer("ws://localhost:3000/cable")
+//   //  // STUPID ACTION CABLE STUFF
+//   // useEffect(()=>{
+//   //   const cable = createConsumer("ws://localhost:3000/cable")
               
-  //   console.log("logging params here", params)
+//   //   console.log("logging params here", params)
 
-  //   const paramsToSend={
-  //     channel: "ConversationChannel",
-  //     id: params.id
-  //   }
+//   //   const paramsToSend={
+//   //     channel: "ConversationChannel",
+//   //     id: params.id
+//   //   }
 
-  //   console.log(paramsToSend)
+//   //   console.log(paramsToSend)
 
-  //   const handlers = {
-  //     received(data){
-  //       setNewMessage([...newMessage, data])
-  //     },
+//   //   const handlers = {
+//   //     received(data){
+//   //       setNewMessage([...newMessage, data])
+//   //     },
 
-  //     connected(){
-  //       console.log("connected")
-  //     },
+//   //     connected(){
+//   //       console.log("connected")
+//   //     },
 
-  //     disconnected(){
-  //       console.log("disconnected")
-  //     }
-  //   }
-  //   const subscription = cable.subscriptions.create(paramsToSend, handlers)
+//   //     disconnected(){
+//   //       console.log("disconnected")
+//   //     }
+//   //   }
+//   //   const subscription = cable.subscriptions.create(paramsToSend, handlers)
 
-  //   return function cleanup(){
-  //     console.log("unsubbing from", params.id)
-  //     subscription.unsubscribe()
-  //   }
-  // }, [params.id, newMessage])
+//   //   return function cleanup(){
+//   //     console.log("unsubbing from", params.id)
+//   //     subscription.unsubscribe()
+//   //   }
+//   // }, [params.id, newMessage])
     
 
 
 
-    function handleNewMessage(e){
-        setNewMessage(e.target.value)
-    }
+//     function handleNewMessage(e){
+//         setNewMessage(e.target.value)
+//     }
 
-    function createMessage(e){
-        e.preventDefault()
-        const newMessageObject ={
-                    content: newMessage,
-                    user_id: user.id,
-                    conversation_id: conversation.id
-                }
-          fetch("/messages", {
-            method: "POST",
-            headers: {
-              "Content-Type": "application/json",
-            },
-            body: JSON.stringify(newMessageObject)
-          }).then(response => response.json()).then(response => setConversation(response))    
-    }
+//     function createMessage(e){
+//         e.preventDefault()
+//         const newMessageObject ={
+//                     content: newMessage,
+//                     user_id: user.id,
+//                     conversation_id: conversation.id
+//                 }
+//           fetch("/messages", {
+//             method: "POST",
+//             headers: {
+//               "Content-Type": "application/json",
+//             },
+//             body: JSON.stringify(newMessageObject)
+//           }).then(response => response.json()).then(response => setConversation(response))    
+//     }
 
-    return (
-        <div id="message-container">
-          {/* <p>{showConversation}</p> */}
-          <p> Chat Here </p>
-            <form id="send-container" onSubmit={createMessage}>
-              <input type="text" id="message-input" onChange={handleNewMessage} value={newMessage} />
-              <button type="submit" id="send-button">Send</button>
-            </form>
-        </div>
-    )
-}
+//     return (
+//         <div id="message-container">
+//           {/* <p>{showConversation}</p> */}
+//           <p> Chat Here </p>
+//             <form id="send-container" onSubmit={createMessage}>
+//               <input type="text" id="message-input" onChange={handleNewMessage} value={newMessage} />
+//               <button type="submit" id="send-button">Send</button>
+//             </form>
+//         </div>
+//     )
+// }
 
-export default Conversation
+// export default Conversation
