a88e1e45e24e533f6b16d4e9fe05bcc0904a2fce
Serje <Serje.havandjian@gmail.com>
2022-10-04 11:19:26 -0500

    changed routes

--- config/routes.rb
+++ config/routes.rb
@@ -11,9 +11,7 @@ Rails.application.routes.draw do
   # root "messages#index"
 
   # Leave this here to help deploy your app later!
-  get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
-
-  get "/", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
+  get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? 
 
   # Route for login after signup
   post "/login", to: "sessions#create"
