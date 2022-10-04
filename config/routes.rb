44c64e37de469a0d11ac52c618e1c6a6f20307fa
Serje <Serje.havandjian@gmail.com>
2022-10-04 00:11:38 -0500

    altering routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,10 +5,12 @@ Rails.application.routes.draw do
   resources :messages
   resources :conversations
   resources :users
+
+  
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  get "/", to: "users#show"
+  root "users#index"
   # Leave this here to help deploy your app later!
   get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
 
