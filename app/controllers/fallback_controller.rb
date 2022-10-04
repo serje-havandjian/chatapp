eaf3e382edd1f8c782a5115d3754563457cc48ff
Serje <Serje.havandjian@gmail.com>
2022-10-04 11:30:50 -0500

    whatever

--- app/controllers/fallback_controller.rb
+++ app/controllers/fallback_controller.rb
@@ -3,7 +3,7 @@ class FallbackController < ActionController::Base
 
     def index
       # React app index page
-      render file: 'public/index.html'
+      render template: 'public/index.html'
     end
   
   end
