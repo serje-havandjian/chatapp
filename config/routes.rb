b85208f0024b4965781c0e5a462d95182f3e7491
Serje <Serje.havandjian@gmail.com>
2022-10-04 11:26:48 -0500

    fallback controller

--- config/routes.rb
+++ config/routes.rb
@@ -8,11 +8,9 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "messages#index"
-
   # Leave this here to help deploy your app later!
   get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
-  
+
   # Route for login after signup
   post "/login", to: "sessions#create"
   delete "/logout", to: "sessions#destroy"
