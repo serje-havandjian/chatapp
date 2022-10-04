3c4a9d4837074bff7f95151aac3deab35fa8888e
Serje <Serje.havandjian@gmail.com>
2022-10-03 23:24:23 -0500

    setup postgres

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,9 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 2022_08_23_003220) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "conversations", force: :cascade do |t|
     t.string "title"
     t.integer "user_a_id"
