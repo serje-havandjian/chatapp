29eaeaa0f975a8a916a865367c004660cb4dde86
Serje <Serje.havandjian@gmail.com>
2022-10-04 00:17:26 -0500

    changed public.html

--- config/routes.rb
+++ config/routes.rb
@@ -5,12 +5,9 @@ Rails.application.routes.draw do
   resources :messages
   resources :conversations
   resources :users
-
-  
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root "/login"
   # Leave this here to help deploy your app later!
   get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
 
@@ -21,6 +18,8 @@ Rails.application.routes.draw do
   # Route for signing up/creating new user
   post "/signup", to: "users#create"
   get "/me", to: "users#show"
+
+  
 
 end
 
