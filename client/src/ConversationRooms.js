6d860d52dd0dea54d49c6ad59de40e04ac7db977
Serje <Serje.havandjian@gmail.com>
2022-10-08 16:54:26 -0700

    logging CR

--- client/src/ConversationRooms.js
+++ client/src/ConversationRooms.js
@@ -48,15 +48,16 @@ function ConversationRooms({user}){
   useEffect(()=>{
     function createSocket(){
 
-     
+      console.log(chatConnection, "CHAT CONNECTION HERE")
+      console.log(chatConnection.consumer, "CONSUMER HERE HERE")
       
-      if(chatConnection.consumer){
-        chatConnection.unsubscribe()
-      }
+      // if(chatConnection.consumer){
+      //   chatConnection.unsubscribe()
+      // }
 
       const consumer = Cable.createConsumer(`wss://chat-app-project-2.herokuapp.com/cable`)
      
-     
+      console.log(consumer, "CONSUMER HERE")
       
       const subscription = consumer.subscriptions.create(
         {
@@ -71,7 +72,7 @@ function ConversationRooms({user}){
       )
       dispatch({ type: "chatConnection", payload: subscription})
 
-     
+      console.log(subscription, "SUBCSCRIPTION HERE")
     }
 
     
@@ -79,7 +80,7 @@ function ConversationRooms({user}){
     if (chatroom.title){
       createSocket()
     }
-  }, [chatroom.title, chatroom.id, chatConnection])
+  }, [chatroom.title, chatroom.id])
 
   useEffect(()=>{
       fetch(`https://chat-app-project-2.herokuapp.com/conversations/${params.id}`)
