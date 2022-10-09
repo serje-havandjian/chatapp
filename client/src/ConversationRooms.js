3578607bfc53697321a16124c464c9e48a3fc5c7
Serje <Serje.havandjian@gmail.com>
2022-10-08 17:31:49 -0700

    changed to ws from wss

--- client/src/ConversationRooms.js
+++ client/src/ConversationRooms.js
@@ -51,7 +51,7 @@ function ConversationRooms({user}){
         chatConnection.unsubscribe()
       }
 
-      const consumer = Cable.createConsumer(`wss://chat-app-project-2.herokuapp.com/cable`)
+      const consumer = Cable.createConsumer(`ws://chat-app-project-2.herokuapp.com/cable`)
      
       console.log(consumer, "CONSUMER HERE")
       
